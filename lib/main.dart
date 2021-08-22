7cec94b470080f2f8ab6c7f8e620edad2bcc1b38
heejeongMin <hj.min1031@gmail.com>
2021-08-23 01:10:37 +0900

    bottomnavi

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:dooromi/Partner/page/PartnerListPage.dart';
+import 'package:dooromi/User/page/LoginPage.dart';
+import 'package:dooromi/Worklog/page/DateAndTimePage.dart';
 import 'package:flutter/material.dart';
 
 import 'User/page/JoinPage.dart';
-import 'Worklog/page/DateAndTimePage.dart';
+
 
 void main() {
   runApp(new MyApp());
@@ -16,8 +19,53 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.teal,
       ),
-      // home: new DateAndTimePage(),
-      home: new JoinPage()
+      home: new MyHomePage()
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _index = 0;
+  var _pages = [
+    JoinPage(),
+    DateAndTimePage(),
+    PartnerListPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          body: _pages[_index],
+          bottomNavigationBar: BottomNavigationBar(
+            onTap: (index){
+              setState(() {
+                _index = index;
+              });
+            },
+            currentIndex: _index,
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.account_circle),
+                  label: '내정보',
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.assignment),
+                  label: '근무일지'
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.people),
+                  label: '거래처관리'
+              ),
+            ],
+          ),
+        )
     );
   }
 }
